a5c51b3489d681a1dac35ca1f3b7aabef724e9ec
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-01 06:29:09 -0700

    I have a lot of repos

--- src/repoman.c
+++ src/repoman.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h> // mkdir()
 
 int repoman(char usage[], char project[], char lang[]) {
   if(!usage && !project && !lang) {
-    fprintf(stderr, "What the heck - there's nothing here, moron.");
+    perror("What the heck - there's nothing here, moron.");
+    exit(EXIT_FAILURE);
   }
   else if(usage[0] == "help") {
-    ; // nothing yet
+    printf("Welcome to repoman by gnuoveryou. There's not much here yet.");
   }
 }
