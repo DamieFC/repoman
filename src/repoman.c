585c1933119208811d5a4055fd4dfc086b7ce975
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-09-05 19:58:01 +0530

    Updated

--- src/repoman.c
+++ src/repoman.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  printf("Repoman Version 0.01\n");
+int main(int argc, char *argv[]) {
+
+  if (argc < 2) {
+    printf("No arguments given.\n");
+    return 1;
+  }
+
+  const char *arg1 = argv[1];
+
+  if (strcmp(arg1, "version") == 0) {
+    printf("Repoman Version 0.01\n");
+  } else {
+    printf("Unknown argument %s\n", arg1);
+    return 1;
+  }
+
   return 0;
 }
