50a27026b5c02da2818de684e901f43a523e5cec
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-09-05 19:46:51 +0530

    Update repoman.c

--- src/repoman.c
+++ src/repoman.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h> // mkdir()
 
-int repoman(char usage[], char project[], char lang[]) {
-  if(!usage && !project && !lang) {
-    perror("What the heck - there's nothing here, moron.");
-    exit(EXIT_FAILURE);
-  }
-  else if(usage[0] == "help") {
-    printf("Welcome to repoman by gnuoveryou. There's not much here yet.");
-  }
+int main() {
+  printf("Repoman Version 0.01\n");
+  return 0;
 }
